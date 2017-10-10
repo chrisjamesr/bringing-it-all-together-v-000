c1b5a77898c45d19f4373d6e8a99e4eb1a9585b1
Chris Rudolph <chrisjames.r@gmail.com>
2017-10-10 01:55:27 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,13 +2,13 @@ require 'pry'
 class Dog
 # attr_accessor :name, :breed
 # attr_reader :id
-# 
+#
 #   def initialize(id: nil, name:, breed:)
 #     @name = name
 #     @breed = breed
 #     @id = id
 #   end
-# 
+#
 #   def self.create_table
 #     sql = <<-SQL
 #       CREATE TABLE IF NOT EXISTS dogs (
@@ -19,30 +19,30 @@ class Dog
 #     SQL
 #     DB[:conn].execute(sql)
 #   end
-# 
+#
 #   def self.drop_table
 #     sql = "DROP TABLE IF EXISTS dogs"
 #     DB[:conn].execute(sql)
 #   end
-# 
+#
 #   def self.create(name:, breed:)
 #     self.new(name:name, breed:breed).save
 #   end
-# 
+#
 #   def self.find_by_id(id)
 #     sql = <<-SQL
 #       SELECT * FROM dogs
 #       WHERE id = ?
 #     SQL
-# 
+#
 #     row = DB[:conn].execute(sql, id)[0]
 #     self.new_from_db(row)
 #   end
-# 
+#
 #   def self.new_from_db(row)
 #      self.new(id:row[0],name:row[1], breed:row[2])
 #   end
-# 
+#
 #   def save
 #     if self.id
 #       self.update
@@ -51,13 +51,13 @@ class Dog
 #         INSERT INTO dogs (name, breed)
 #         VALUES (?,?)
 #       SQL
-# 
+#
 #       DB[:conn].execute(sql, self.name, self.breed)
 #       @id = DB[:conn].execute("SELECT id FROM dogs WHERE name = ?", self.name)[0].first
 #     end
 #     self
 #   end
-# 
+#
 #   def self.find_by_name(name)
 #     sql = <<-SQL
 #       SELECT * from dogs
@@ -68,7 +68,7 @@ class Dog
 #       self.new_from_db(row)
 #     end
 #   end
-# 
+#
 #   def self.find_or_create_by(name:, breed:)
 #     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = '#{name}' AND breed = '#{breed}'")
 #     if !dog.empty?
@@ -78,14 +78,14 @@ class Dog
 #       self.create(name:name, breed:breed)
 #     end
 #   end
-# 
+#
 #   def update
 #     sql = <<-SQL
 #       UPDATE dogs
 #       SET name = ?, breed = ?
 #       WHERE id = ?
 #     SQL
-# 
+#
 #     DB[:conn].execute(sql, self.name, self.breed, self.id)
 #   end
 
