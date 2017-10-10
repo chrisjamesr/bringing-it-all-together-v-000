46ec46d9d5525c6f12e0985357f490ff8dd51ea6
Chris Rudolph <chrisjames.r@gmail.com>
2017-10-10 01:54:47 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -17,7 +17,6 @@ attr_reader :id
         breed TEXT
       )
     SQL
-
     DB[:conn].execute(sql)
   end
 
@@ -70,15 +69,25 @@ attr_reader :id
     end
   end
 
+  # def self.find_or_create_by(name:, breed:)
+  #   dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = '#{name}' AND breed = '#{breed}'")
+  #   if !dog.empty?
+  #     row = dog[0]
+  #     self.new_from_db(row)
+  #   else
+  #     self.create(name:name, breed:breed)
+  #   end
+  # end
   def self.find_or_create_by(name:, breed:)
-    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = '#{name}' AND breed = '#{breed}'")
-    if !dog.empty?
-      row = dog[0]
-        self.new_from_db(row)
-    else
-        self.create(name:name, breed:breed)
-    end
-  end
+   dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = '#{name}' AND breed = '#{breed}'")
+   if !dog.empty?
+     dog_data = dog[0]
+     dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+   else
+     dog = self.create(name: name, breed: breed)
+   end
+   dog
+ end
 
   def update
     sql = <<-SQL
